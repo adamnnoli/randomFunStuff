789332c68de082d63c615e22049a5cd6bab32cc4
Adam <aon2@cornell.edu>
2020-05-03 19:20:58 -0400

    Reworked algorithm
    -Now prompts for input, fixed a bug, added documentation

--- eratosthenesSieve.py
+++ eratosthenesSieve.py
@@ -1,15 +1,34 @@
+"""
+Implements the pseudocode for the Eratosthenes Sieve for 
+generating prime numbers
+"""
+
 import math
 
+"""
+Generates list of all prime numbers from 2 to n
+Requires: n is an integer
+"""
 def eratosthenesSieve(n):
   temp = [True for i in range(2,n+1)]
-  j = 0
+  
+  j = 2
   while j < math.sqrt(n):
     if temp[j]:
-      temp[j] = False
+      k = j*j
+      while k < n: 
+        temp[k] = False
+        k += j
     j += 1
+  
   result = []
-  for i in range(n):
+  for i in range(2, n-1):
     if temp[i]:
       result.append(i)
+  
+  return result
+
+#Prompt user for input number and print all primes up to that number
+#Requires: Input is an integer
 if __name__ == "__main__":
-  print(eratosthenesSieve(30))
+  print(eratosthenesSieve(int(input())))
